5d230f1748cb82f580f416acedf15230a8a03bdb
terrycojones <terry@fluidinfo.com>
2012-05-16 23:06:37 +0100

    Ripped out display of user's tags. Now only one notification will pop up, showing only known domains.

--- notification.js
+++ notification.js
@@ -1,51 +1,14 @@
 // Note: customDisplayPrefixes is already in scope in this file.
 // It's a global, declared in domains/domains.js
 
-var _getCustomPrefixesOnObject = function(tagPaths){
-    var prefixes = {};
-    for (prefix in customDisplayPrefixes){
-        if (customDisplayPrefixes.hasOwnProperty(prefix)){
-            for (var i = 0; i < tagPaths.length; i++){
-                if (tagPaths[i].slice(0, prefix.length) === prefix){
-                    // This is one of the prefixes in customDisplayPrefixes
-                    prefixes[prefix] = true;
-                }
-            }
-        }
-    }
-    return prefixes;
-};
-
-var _filterCustomPrefixesFromTagList = function(tagPaths, customPrefixes){
-    var resultTags = [];
-    for (var i = 0; i < tagPaths.length; i++){
-        var tagPath = tagPaths[i];
-        var foundCustom = false;
-        for (prefix in customDisplayPrefixes){
-            if (customDisplayPrefixes.hasOwnProperty(prefix)){
-                if (tagPath.slice(0, prefix.length) === prefix){
-                    foundCustom = true;
-                    break;
-                }
-            }
-        }
-        if (!foundCustom){
-            resultTags.push(tagPath);
-        }
-    }
-    return resultTags;
-};
 
 var populate = function(options){
     /*
      * options contains:
      *     about
-     *     dropNamespaces
-     *     loggedIn
-     *     title
+     *     knownPrefixes
      *     tabId
      *     tagValueHandler
-     *     wantedTags
      */
     var about = options.about;
     var truncatedAbout = valueUtils.truncateAbout(about, 35);
@@ -58,8 +21,6 @@ var populate = function(options){
         action: 'show sidebar'
     });
 
-    document.getElementById('fi_login').style.display = (options.loggedIn ? 'none' : '');
-    document.getElementById('fi_title').innerHTML = options.title;
     document.getElementById('fi_url').innerHTML = Mustache.render(
         '<a href="{{url}}" target="_blank">{{truncatedAbout}}</a>', {
             truncatedAbout: truncatedAbout,
@@ -67,38 +28,13 @@ var populate = function(options){
         }
     );
 
-    var customPrefixesOnObject = _getCustomPrefixesOnObject(options.wantedTags);
-    var regularTags = _filterCustomPrefixesFromTagList(options.wantedTags,
-                                                       customPrefixesOnObject);
-    var content = [];
-
     // Add the HTML for each of the custom prefixes on the object.
-    var prefix;
-    for (prefix in customPrefixesOnObject){
-        if (customPrefixesOnObject.hasOwnProperty(prefix)){
-            var func = customDisplayPrefixes[prefix];
-            var tagValues = options.tagValueHandler.cache;
-            content.push(func(tagValues, about));
-        }
-    }
-
-    // Add the regular HTML for the tags that are not part of a custom prefix.
-    try {
-        var regularContent = valueUtils.pathsAndValuesToHTML({
-            displayImages: false,
-            dropNamespaces: options.dropNamespaces,
-            fetchLinks: false,
-            linkTags: true,
-            result: { data: options.tagValueHandler.cache },
-            showTagPaths: true,
-            tagPaths: regularTags
-        });
-
-        content.push(regularContent.content);
-    }
-    catch (error){
-        console.log('Hit an error rendering regular tag values.');
-        console.log(error.message);
+    var content = [];
+    for (var i = 0; i < options.knownPrefixes.length; i++){
+        var prefix = options.knownPrefixes[i];
+        var func = customDisplayPrefixes[prefix];
+        var tagValues = options.tagValueHandler.cache;
+        content.push(func(tagValues, about));
     }
 
     document.getElementById('fi_tags').innerHTML = content.join('');
