1e813caec71270e5e54b0032e8191be13ffb0ea6
terrycojones <terry@fluidinfo.com>
2012-05-28 10:56:12 +0100

    Added view in sidebar option to context menus.

--- background.js
+++ background.js
@@ -48,6 +48,17 @@ function openNewTab(about, info, tab){
     });
 }
 
+function openInSidebar(about, info, tab){
+    /*
+     * Open the sidebar, looking at the given about value.
+     */
+    var port = chrome.tabs.connect(tab.id, {name: 'sidebar'});
+    port.postMessage({
+        about: about,
+        action: 'show sidebar'
+    });
+}
+
 function makeURL(about, info){
     /*
      * Generate an object browser URL given an about value and an info
@@ -73,13 +84,23 @@ function refererFragment(info){
 // --------------------------- Page --------------------------
 
 chrome.contextMenus.create({
-    'title' : 'Go to this page',
+    'title' : 'This page in the sidebar',
+    'type' : 'normal',
+    'contexts' : ['page'],
+    'onclick' : function(info, tab){
+        openInSidebar(info.pageUrl, info, tab);
+    }
+});
+
+chrome.contextMenus.create({
+    'title' : 'This page on fluidinfo.com',
     'type' : 'normal',
     'contexts' : ['page'],
     'onclick' : function(info, tab){
         openNewTab(info.pageUrl, info, tab);
     }
 });
+
 
 var absoluteHref = function(linkURL, docURL){
     /*
@@ -123,8 +144,16 @@ var addContextMenuItem = function(text, context){
     text = (text.length < 50 ? text : text.slice(0, 47) + '...').replace(/\n+/g, ' ');
 
     if (typeof contextMenuItems[text] === 'undefined'){
+        var sidebarMenuItem = chrome.contextMenus.create({
+            'title' : text + ' in the sidebar',
+            'type' : 'normal',
+            'contexts' : [context],
+            'onclick' : function(info, tab){
+                openInSidebar(text, info, tab);
+            }
+        });
         var gotoMenuItem = chrome.contextMenus.create({
-            'title' : 'Go to ' + text,
+            'title' : text + ' on fluidinfo.com',
             'type' : 'normal',
             'contexts' : [context],
             'onclick' : function(info, tab){
@@ -133,7 +162,8 @@ var addContextMenuItem = function(text, context){
         });
         contextMenuItems[text] = {
             context: context,
-            gotoMenuItem: gotoMenuItem
+            gotoMenuItem: gotoMenuItem,
+            sidebarMenuItem: sidebarMenuItem
         };
     }
 };
@@ -144,6 +174,7 @@ var removeContextMenuItemsByContext = function(context){
         if (typeof contextMenuItems[text] !== 'undefined' &&
             contextMenuItems[text].context === context){
             chrome.contextMenus.remove(contextMenuItems[text].gotoMenuItem);
+            chrome.contextMenus.remove(contextMenuItems[text].sidebarMenuItem);
             delete contextMenuItems[text];
         }
     }
@@ -290,7 +321,16 @@ chrome.extension.onConnect.addListener(function(port){
 // --------------------------- Image --------------------------
 
 chrome.contextMenus.create({
-    'title' : 'Go to this image',
+    'title' : 'This image in the sidebar',
+    'type' : 'normal',
+    'contexts' : ['image'],
+    'onclick' : function(info, tab){
+        openInSidebar(info.srcUrl, info, tab);
+    }
+});
+
+chrome.contextMenus.create({
+    'title' : 'This image on fluidinfo.com',
     'type' : 'normal',
     'contexts' : ['image'],
     'onclick' : function(info, tab){
