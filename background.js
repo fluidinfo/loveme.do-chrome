c1338f7f8a11f383a4ff1d78e5eff4c3fe77f903
terrycojones <terry@fluidinfo.com>
2012-08-27 22:35:11 -0400

    TRIVIAL: use http not https for calls to fluiddb until we have our certificate updated.

--- background.js
+++ background.js
@@ -24,7 +24,7 @@ var settings = new Store('settings', {
     sidebarWidth: 300
 });
 
-var anonFluidinfoAPI = fluidinfo({instance: 'https://' + fluidDBHost + '/'});
+var anonFluidinfoAPI = fluidinfo({instance: 'http://' + fluidDBHost + '/'});
 
 var absoluteHref = function(linkURL, docURL){
     /*
